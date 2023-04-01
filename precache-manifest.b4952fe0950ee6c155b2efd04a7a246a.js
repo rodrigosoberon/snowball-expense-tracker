40435f9f5b3a57759ab4e26da5927cf4a996a9c2
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2023-04-01 16:21:40 -0300

    Updates

---
+++ precache-manifest.b4952fe0950ee6c155b2efd04a7a246a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068a552bb083d8f87524999eb9a451d0",
+    "url": "/snowball-expense-tracker/index.html"
+  },
+  {
+    "revision": "cd96e2335e3d29ac31fc",
+    "url": "/snowball-expense-tracker/static/css/main.9663ae01.chunk.css"
+  },
+  {
+    "revision": "a919c41e10d57b85ca06",
+    "url": "/snowball-expense-tracker/static/js/2.62f79bd5.chunk.js"
+  },
+  {
+    "revision": "cd96e2335e3d29ac31fc",
+    "url": "/snowball-expense-tracker/static/js/main.1510c05c.chunk.js"
+  },
+  {
+    "revision": "2c929bcdf7aca195479e",
+    "url": "/snowball-expense-tracker/static/js/runtime-main.bf63523d.js"
+  },
+  {
+    "revision": "8c7b748970595866141824e0ae783ff2",
+    "url": "/snowball-expense-tracker/static/media/edit.8c7b7489.svg"
+  },
+  {
+    "revision": "8db4e757ae6c3d66dfb70339911a8eef",
+    "url": "/snowball-expense-tracker/static/media/trash.8db4e757.svg"
+  },
+  {
+    "revision": "f928e27b6513d0d9c25a1b80293b12d1",
+    "url": "/snowball-expense-tracker/static/media/under-construction.f928e27b.png"
+  }
+]);
