b217b612b109c08256897a2da20151916bfd0697
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2023-04-01 17:17:09 -0300

    image ulr fix

--- frontend/src/shared/pages/Welcome.js
+++ frontend/src/shared/pages/Welcome.js
@@ -6,7 +6,7 @@ import './Welcome.css'
 const Welcome = () => {
 	return (
 		<div className='welcome'>
-			<img src='/snowman.png' alt='Snowman logo' />
+			<img src='./snowman.png' alt='Snowman logo' />
 			<h1>Welcome to</h1>
 			<h1 className='title'>Snowball Expense Tracker</h1>
 			<p>
